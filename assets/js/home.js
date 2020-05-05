a2f1f2224e9ae39da1fc9ddd436bf6a7844a5fc9
Rahul Madhavan <rahul.maddy@gmail.com>
2020-05-05 21:54:39 +0530

    fonts async

--- assets/js/home.js
+++ assets/js/home.js
@@ -58,78 +58,83 @@ function loadHome(dirName) {
 }
 function getSearchString(){return window.location.search;}
 
+function loadIntroText(introResponse){
+    //Introduction
+    const introPart = document.createElement("div");
+    introPart.className = "home-text"
+    para = introResponse.responseText;
+    paraS = para.split("~~~");
+
+    for(let i =0;i<paraS.length;i++){
+        let para = document.createElement("div");
+        para.className = "home-textpara";
+        para.innerHTML = paraS[i];
+        introPart.appendChild(para);
+    }
+    document.getElementById("Home").appendChild(introPart);
+}
+function loadCollaborators(collabResponse){
+    //Collaborators Heading
+    const collabHeading = document.createElement("div");
+    collabHeading.className = "home-header";
+    collabHeading.innerHTML = "Collaborators";
+    document.getElementById("Home").appendChild(collabHeading);
+
+    //Create Page
+    const institutes = document.createElement("div");
+    institutes.className= "home-text";
+    institutes.id = "institutions"
+    instisData = collabResponse.responseText;
+    instisDataS = instisData.split("+++");
+    for (let i = 0;i<instisDataS.length;i++){
+        let institute = document.createElement("div");
+        institute.className = "institute";
+
+        let instiData =  instisDataS[i].trim();
+        let instiDataS =  instiData.split("\n");
+
+        let instituteName = document.createElement("div");
+        instituteName.className = "institute-name";
+        let instituteLink = document.createElement('a');
+        instituteLink.className = "institute-name-link";
+        instituteLink.target = "_blank";
+        instiNameSplit = instiDataS[0].split("||");
+        instituteLink.href = instiNameSplit[1];
+        instituteLink.innerHTML = instiNameSplit[0];
+        instituteName.appendChild(instituteLink);
+        institute.appendChild(instituteName);
+        uldiv = document.createElement("ul");
+        for(let j =1;j<instiDataS.length;j++){
+            let lidiv = document.createElement("li");
+            let personName = document.createElement("a");
+            personName.className = "name";
+            personName.target = "_blank";
+            nameSplit = instiDataS[j].split("||");
+            personName.href = nameSplit[1];
+            personName.innerHTML = nameSplit[0];
+            lidiv.appendChild(personName);
+            uldiv.appendChild(lidiv);
+        }
+        if(instiDataS.length>1){
+            institute.appendChild(uldiv);
+        }
+        institutes.appendChild(institute);
+    }
+    document.getElementById("Home").appendChild(institutes);
+}
+
 function generateHomeHTML(dirName){
     //HomePage
     const introhttp = loadFileAsync(dirName+"home/intro.txt");
+    const collaboratorshttp = loadFileAsync(dirName+"home/collaborators.txt");
     introhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-
-            //Introduction
-            const introPart = document.createElement("div");
-            introPart.className = "home-text"
-            para = this.responseText;
-            paraS = para.split("~~~");
-
-            for(let i =0;i<paraS.length;i++){
-                let para = document.createElement("div");
-                para.className = "home-textpara";
-                para.innerHTML = paraS[i];
-                introPart.appendChild(para);
-            }
-            document.getElementById("Home").appendChild(introPart);
-        }
-    }
-    const collaboratorshttp = loadFileAsync(dirName+"home/collaborators.txt");
-    collaboratorshttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            //Collaborators Heading
-            const collabHeading = document.createElement("div");
-            collabHeading.className = "home-header";
-            collabHeading.innerHTML = "Collaborators";
-            document.getElementById("Home").appendChild(collabHeading);
-
-            //Create Page
-            const institutes = document.createElement("div");
-            institutes.className= "home-text";
-            institutes.id = "institutions"
-            instisData = this.responseText;
-            instisDataS = instisData.split("+++");
-            for (let i = 0;i<instisDataS.length;i++){
-                let institute = document.createElement("div");
-                institute.className = "institute";
-
-                let instiData =  instisDataS[i].trim();
-                let instiDataS =  instiData.split("\n");
-
-                let instituteName = document.createElement("div");
-                instituteName.className = "institute-name";
-                let instituteLink = document.createElement('a');
-                instituteLink.className = "institute-name-link";
-                instituteLink.target = "_blank";
-                instiNameSplit = instiDataS[0].split("||");
-                instituteLink.href = instiNameSplit[1];
-                instituteLink.innerHTML = instiNameSplit[0];
-                instituteName.appendChild(instituteLink);
-                institute.appendChild(instituteName);
-                uldiv = document.createElement("ul");
-                for(let j =1;j<instiDataS.length;j++){
-                    let lidiv = document.createElement("li");
-                    let personName = document.createElement("a");
-                    personName.className = "name";
-                    personName.target = "_blank";
-                    nameSplit = instiDataS[j].split("||");
-                    personName.href = nameSplit[1];
-                    personName.innerHTML = nameSplit[0];
-                    lidiv.appendChild(personName);
-                    uldiv.appendChild(lidiv);
+            loadIntroText(this);
+            collaboratorshttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    loadCollaborators(this);
                 }
-                if(instiDataS.length>1){
-                    institute.appendChild(uldiv);
-                }
-                institutes.appendChild(institute);
             }
-            document.getElementById("Home").appendChild(institutes);
-
         }
     }
 }
