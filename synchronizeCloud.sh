fd89433beebafb1a1c507b6ff97e55cab80ae411
Rahul Madhavan <rahul.maddy@gmail.com>
2023-11-13 16:08:03 +0530

    content changes

--- synchronizeCloud.sh
+++ synchronizeCloud.sh
@@ -11,3 +11,16 @@ echo "Base Directory Synced"
 git add -A
 git commit -m "content changes"
 git push
+
+# Now attempt to directly update website through SSH
+# Ensure local computer's ssh credentials are added to remote machine's ~/.ssh/authorized_keys
+ssh -t polyalg@csacloud.iisc.ac.in -p 3232 "./syncPolyAlg.sh"
+echo "Please check that the website https://polyalg.csa.iisc.ac.in/ is updated."
+# If not, use:
+plink polyalg@csacloud.iisc.ac.in -P 3232 -pw P0ly@lg879
+./syncPolyAlg.sh
+echo "Please check that the website https://polyalg.csa.iisc.ac.in/ is updated"
+exit
+
+
+# exit
